ea0d316071f2f96a0417d646c45c5dfabdc26fcb
Sergio Silva <sergiosilva@nasajon.com.br>
2022-03-31 16:56:02 -0300

    Utilitario para formatacao dos erros

---
+++ tests/rest_error_util_test.py
@@ -0,0 +1,43 @@
+import unittest
+from nsj_gcf_utils.exception import ERPException
+
+from nsj_gcf_utils.rest_error_util import format_error_body
+
+
+class RestErrorUtilTest(unittest.TestCase):
+
+    def test_format_tuple(self):
+        error = ('00', 'msg')
+        formated = format_error_body(error)
+
+        self.assertEquals(formated[0]['code'], '00')
+        self.assertEquals(formated[0]['message'], 'msg')
+
+    def test_format_erpexception(self):
+        error = ERPException('00', 'msg')
+        formated = format_error_body(error)
+
+        self.assertEquals(formated[0]['code'], '00')
+        self.assertEquals(formated[0]['message'], 'msg')
+
+    def test_format_list_tuple(self):
+        error = [('00', 'msg'), ('01', 'msg1')]
+        formated = format_error_body(error)
+
+        self.assertEquals(formated[0]['code'], '00')
+        self.assertEquals(formated[0]['message'], 'msg')
+        self.assertEquals(formated[1]['code'], '01')
+        self.assertEquals(formated[1]['message'], 'msg1')
+
+    def test_format_list_erpexception(self):
+        error = [ERPException('00', 'msg'), ERPException('01', 'msg1')]
+        formated = format_error_body(error)
+
+        self.assertEquals(formated[0]['code'], '00')
+        self.assertEquals(formated[0]['message'], 'msg')
+        self.assertEquals(formated[1]['code'], '01')
+        self.assertEquals(formated[1]['message'], 'msg1')
+
+
+if __name__ == '__main__':
+    unittest.main()
