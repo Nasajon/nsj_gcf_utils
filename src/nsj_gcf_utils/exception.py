ab3a3471667f76b7f2c16d70b78f5e6983a7110c
Sergio Silva <sergiosilva@nasajon.com.br>
2022-03-31 18:23:38 -0300

    Criaçao da excepçao NotFound

--- src/nsj_gcf_utils/exception.py
+++ src/nsj_gcf_utils/exception.py
@@ -12,3 +12,7 @@ class PaginationException(ERPException):
     def __init__(self, msg: str):
         super().__init__('0000-E001',
                          f'Erro nos parâmetros requisitados para paginação: {msg}')
+
+
+class NotFoundException(Exception):
+    pass
